de8bcf2abad68de61dd33f5867bf7b25aa9eee16
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-12 03:50:06 -0500

    triggerer

--- fp_anime.py
+++ fp_anime.py
@@ -4,9 +4,13 @@ from anime_stream import download_anime
 
 anime_data = {}
 
+def trigger():
+    download_anime(anime_data)
+
 class Anime:
-	@bot.command()
-	async def anime(ctx, _anime: str, _ep: int):
-		anime_data["anime"] = _anime
-		anime_data["episode"] = _ep
-		download_anime(anime_data)
+    @bot.command()
+    async def anime(ctx, _anime: str, _ep: int):
+        anime_data["anime"] = _anime
+        anime_data["episode"] = _ep
+
+        trigger()
