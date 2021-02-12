a10c168317702d4a7f65e1be378e8d20bc919ec1
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-11 23:35:48 -0500

    anime prototype

--- fp_anime_stream.py
+++ fp_anime_stream.py
@@ -1,13 +1,26 @@
 import discord
 from bot_settup import bot
-import chromedriver_binary
-from selenium import webdriver
+from Sakurajima import Sakurajima
+
+client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
 
 class Anime:
     @bot.command()
-    async def test(ctx): 
-        driver = webdriver.Chrome("C:/Users/baili/OneDrive/Desktop/chromedriver.exe")
-        driver.get("http://www.python.org")
-        assert "Python" in driver.title
-        await ctx.channel.purge(ctx)
-        await ctx.send("Done! Hopefully...")
+    def searchanime(ctx, anime, episode):
+        if client.search(anime) == []:
+            await ctx.send(f"{anime} isn't in my list, try spelling ")
+
+        else:
+            try:
+                await ctx.send(f"Here are the results for {anime}:\n{client.search(anime)}")
+
+    @bot.command()
+    def playanime(ctx, anime, episode)
+    try:
+        my_anime = client.search(anime)[0]
+        all_episodes = my_anime.get_episodes()
+        episode = all_episodes.get_episode_by_number(episode) 
+        episode.download("fullhd", "anime", "C:/Programming/FlyingPig/animes")
+    
+    except IndexError:
+        await ctx.send(f"I don't have episode {episode} of {anime}. Sorry :P")
