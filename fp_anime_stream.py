695642e2fbaa7bec101da169314d184fe130a4ea
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-12 02:46:16 -0500

    dwnwehbfjhb

--- fp_anime_stream.py
+++ fp_anime_stream.py
@@ -4,16 +4,16 @@ from Sakurajima import Sakurajima
 
 class Anime:
     @bot.command()
-    async def searchanime(ctx, anime: str):
+    def searchanime(ctx, anime: str):
         client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
         if client.search(anime) == []:
-            await ctx.send(f"{anime} isn't in my list, try spelling ")
+            ctx.send(f"{anime} isn't in my list, try spelling ")
 
         else:
-            await ctx.send(f"Here are the results for {anime}:\n{client.search(anime)}")
+            ctx.send(f"Here are the results for {anime}:\n{client.search(anime)}")
 
     @bot.command()
-    async def playanime(ctx, anime, episode):
+    def playanime(ctx, anime, episode):
         client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
         try:
             my_anime = client.search(anime)[0]
@@ -22,4 +22,4 @@ class Anime:
             episode.download("fullhd", "anime", "C:/Users/baili/OneDrive/Desktop/animes")
         
         except IndexError:
-            await ctx.send(f"I don't have episode {episode} of {anime}. Sorry :P")
+            ctx.send(f"I don't have episode {episode} of {anime}. Sorry :P")
