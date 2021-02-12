5bbc2fb1cd03f0d635f5e1fcc289ec3ab09354a9
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-12 03:40:25 -0500

    jnfhwejf

--- anime_stream.py
+++ anime_stream.py
@@ -1,8 +1,8 @@
 from Sakurajima import Sakurajima
 
-def download_anime(anime_data):
+async def download_anime(anime_data):
     client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
     my_anime = client.search(anime_data["anime"])[0]
     all_episodes = my_anime.get_episodes()
     episode = all_episodes.get_episode_by_number(anime_data["episode"]) 
-    episode.download("fullhd", "anime", "C:/Users/baili/OneDrive/Desktop/animes")
+    episode.download("fullhd", "anime", "animes")
