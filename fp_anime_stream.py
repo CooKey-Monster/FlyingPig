b704b1108fd356f38055a8ac33ad266d6e30bd36
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-12 01:06:03 -0500

    test

--- fp_anime_stream.py
+++ fp_anime_stream.py
@@ -6,17 +6,26 @@ client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiR
 
 class Anime:
     @bot.command()
-    def searchanime(ctx, anime: str):
-        print("Started to get the client")
-        
+    async def test(ctx):
         global client
-        if client.search(anime) == []:
-            print(f"{anime} not found")
-
-        else:
-            print(client.search(anime))
-
+        await ctx.send(client.search("naruto"))
     '''@bot.command()
+    async def searchanime(ctx, anime):
+        print("Started to get the client")
+        global client
+        print("got the client")
+        if client.search(anime) == []:
+            print("I don't have results for this anime :(")
+            await ctx.send(f"{anime} isn't in my list, try spelling ")
+        
+        else:
+            print("I have results for anime!")
+            await ctx.send(f"Here are the results for {anime}:\n{client.search(anime)}")
+
+        print("command done!")
+
+
+    @bot.command()
     async def playanime(ctx, anime, episode):
         global client
         try:
