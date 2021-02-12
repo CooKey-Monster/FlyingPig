debe18a07dd9dc8f1e51320be6b0c2f742807481
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-12 02:30:11 -0500

    test

--- fp_anime_stream.py
+++ fp_anime_stream.py
@@ -2,38 +2,19 @@ import discord
 from bot_settup import bot
 from Sakurajima import Sakurajima
 
-client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
-
-def get_anime(anime: str):
-    return client.search(anime)
-
-aniime = get_anime("naruto")
-
 class Anime:
     @bot.command()
-    async def test(ctx):
-        global aniime
-        await ctx.send("FLIP THIS WORLD")
-        await ctx.send(aniime)
-    '''@bot.command()
-    async def searchanime(ctx, anime):
-        print("Started to get the client")
-        global client
-        print("got the client")
+    async def searchanime(ctx, anime: str):
+        client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
         if client.search(anime) == []:
-            print("I don't have results for this anime :(")
             await ctx.send(f"{anime} isn't in my list, try spelling ")
-        
+
         else:
-            print("I have results for anime!")
             await ctx.send(f"Here are the results for {anime}:\n{client.search(anime)}")
-
-        print("command done!")
-
 
     @bot.command()
     async def playanime(ctx, anime, episode):
-        global client
+        client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
         try:
             my_anime = client.search(anime)[0]
             all_episodes = my_anime.get_episodes()
@@ -41,4 +22,4 @@ class Anime:
             episode.download("fullhd", "anime", "C:/Users/baili/OneDrive/Desktop/animes")
         
         except IndexError:
-            await ctx.send(f"I don't have episode {episode} of {anime}. Sorry :P")'''
+            await ctx.send(f"I don't have episode {episode} of {anime}. Sorry :P")
