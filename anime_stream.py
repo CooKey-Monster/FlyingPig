466563132f683009b57d00467d5e2b23a1d5cbad
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-12 03:27:18 -0500

    storing in a dict the anime data

--- anime_stream.py
+++ anime_stream.py
@@ -1,8 +1,9 @@
+from fp_anime import anime_data
 from Sakurajima import Sakurajima
 
 def download_anime(anime):
     client = Sakurajima("ExplosiveDiarrheaPig", "927950", "Z3FsY2QvaFNSYUJXWHErcmhiRDdtZz09")
-    my_anime = client.search(anime)[0]
+    my_anime = client.search(anime_data["anime"])[0]
     all_episodes = my_anime.get_episodes()
-    episode = all_episodes.get_episode_by_number(episode) 
+    episode = all_episodes.get_episode_by_number(anime_data["episode"]) 
     episode.download("fullhd", "anime", "C:/Users/baili/OneDrive/Desktop/animes")
