00c0bc63fa4c3e4ede0320e1a130ff6e885dc9a1
vrroooomm <explosivediarrheapig@gmail.com>
2021-02-16 04:39:39 -0500

    added anime file

---
+++ fp_anime.py
@@ -0,0 +1,16 @@
+import discord
+from bot_settup import bot
+from ApiWrapper import Wrapper
+
+class Anime:
+    @bot.command()
+    async def anime(ctx,*,anime):
+        api = Wrapper()
+        result = api.search_anime(anime)
+        embed = discord.Embed(title = "Anime Results",description = f"Search Results for {anime}",color = discord.Color.green())
+        i = 0
+        for anime in result:
+            embed.add_field(name = f"{i}.",value = f":diamond_shape_with_a_dot_inside: **{anime}**",inline = False)
+            i += 1
+
+        await ctx.send(embed = embed, content = ctx.author.mention)
